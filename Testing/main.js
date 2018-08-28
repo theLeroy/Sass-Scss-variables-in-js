3cc601956a1f523c135cb4aadf0a763b442678cd
Leroy <4c65726f79@gmail.com>
2018-08-28 23:49:14 +0200

    Problems with the Readout unknown Properties

---
+++ Testing/main.js
@@ -0,0 +1,21 @@
+function ImportScssVariablesToJs(Filepath) {
+//   var elem,
+//     style;
+// elem = document.querySelector('ExportToJs');
+// style = getComputedStyle(elem);
+// style.varTestvariable1; //`20px`
+// style.marginRight; //`20px`
+// style.marginBottom; //`20px`
+// style.marginLeft; //`20px`
+// console.log(style);
+// console.log(style.varTestvariable1);
+
+
+
+// document.styleSheets[0];
+
+// console.log(document.styleSheets[0].media.mediaText)
+}
+
+
+//https://stackoverflow.com/questions/52066578/get-raw-style-sheets-values
